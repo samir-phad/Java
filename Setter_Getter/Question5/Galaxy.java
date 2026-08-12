7cd5af5045d25be4fd3c0a32be809b88e245b888
Samir9518 <samirphad4076@gmail.com>
2026-08-12 23:54:31 +0530

    Add Question5 School setter getter

---
+++ Setter_Getter/Question5/Galaxy.java
@@ -0,0 +1,33 @@
+public class Galaxy{
+
+
+	private String gName;
+	private double gArea;
+	private long gDistance;
+
+
+	public void setGname(String name){
+	this.gName = name;
+	}
+	
+	public String getGname(){
+	return gName;
+	}
+
+	public double setGarea(double area){
+	this.gArea = area;
+	}
+	
+	public double getGarea(){
+	return gArea;
+	}
+
+	public void setGdistance(long distance){
+	this.gDistance = distance;
+	}
+	
+	public long getGdistance(){
+	return gDistance;
+	}
+
+}
---
+++ Setter_Getter/Question5/GalaxyInformation.java
@@ -0,0 +1,19 @@
+public class GalaxyInformation{
+
+
+public static void main (String [] args){
+
+
+	Galaxy g = new Galaxy();
+	g.setGname("Jupiter");
+	System.ont.println("Galaxy Name is :- " + g.getGname());
+	
+	g.setGarea(6585);
+	System.ont.println("Galaxy area is :- " + g.getGarea());
+
+	g.setGdistance(9856235);
+	System.ont.println("Galaxy Distance is :- " + g.getGdistance());
+
+}
+
+}
