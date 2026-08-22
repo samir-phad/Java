a1073f22c94e2756754e296ca31034ff67fbfff8
Samir Phad <samirphad4076@gmail.com>
2026-08-22 23:44:31 +0530

    Add Star6 pattern

---
+++ DO-While/Star-Pattern1/Star6.java
@@ -0,0 +1,23 @@
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+
+public class Star6 {
+    public static void main(String[] args) {
+
+    for (int i = 1; i <= 5; i++) {
+
+        for (int j = 1; j <= 5 - i; j++) {
+            System.out.print(" ");
+        }
+
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            System.out.print("*");
+        }
+
+        System.out.println();
+        }
+    }
+}
