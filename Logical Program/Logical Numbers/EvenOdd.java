87de42ae2e288f80c2e28e4a64026cef5b1d66d2
Samir Phad <samirphad4076@gmail.com>
2026-08-23 23:19:12 +0530

    Add Even Odd logical program

---
+++ Logical Program/Logical Numbers/EvenOdd.java
@@ -0,0 +1,13 @@
+// Check number is Even or Odd
+public class EvenOdd {
+    public static void main(String[] args) {
+
+        int n = 10;
+
+        if (n % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }  
+}
