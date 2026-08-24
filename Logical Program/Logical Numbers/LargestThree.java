c008460994383f119a4eed7faaedc390d5765435
Samir Phad <samirphad4076@gmail.com>
2026-08-24 23:14:42 +0530

    Add Largest between Three Number logical program

---
+++ Logical Program/Logical Numbers/LargestThree.java
@@ -0,0 +1,16 @@
+public class LargestThree {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 25;
+        int c = 15;
+
+        if (a > b && a > c) {
+            System.out.println(a + " is largest");
+        } else if (b > a && b > c) {
+            System.out.println(b + " is largest");
+        } else {
+            System.out.println(c + " is largest");
+        }
+    }
+}
