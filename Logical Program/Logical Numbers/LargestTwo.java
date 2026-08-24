e69573b2928c1e954446d45a4f3c6f4a582dda04
Samir Phad <samirphad4076@gmail.com>
2026-08-24 23:13:38 +0530

    Add Largest between Two Number logical program

---
+++ Logical Program/Logical Numbers/LargestTwo.java
@@ -0,0 +1,13 @@
+public class LargestTwo {
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 15;
+
+        if (a > b) {
+            System.out.println(a + " is largest");
+        } else {
+            System.out.println(b + " is largest");
+        }
+    }
+}
