dfe4b79dd4b867cab2fe35d44dfb0c31600ee048
Samir Phad <samirphad4076@gmail.com>
2026-08-24 23:10:38 +0530

    Add Positive Number logical program

---
+++ Logical Program/Logical Numbers/PositiveN.java
@@ -0,0 +1,15 @@
+public class PositiveN {
+    public static void main(String[] args) {
+
+        int n = 15;
+
+        if (n > 0) {
+            System.out.println("Positive");
+        } else if (n < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+
+}
