37c74a2c212fbad04afb09a8b5cb1937b8de4c44
Samir Phad <samirphad4076@gmail.com>
2026-08-26 16:52:28 +0530

    Add Check Prime Number between Given Number logical program

---
+++ Logical Program/Logical Numbers/PrimeN.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class PrimeN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number :- ");
+        int n = sc.nextInt();
+
+        for(int i = 1; i <n ; i++){
+            int count = 0;
+            for(int j = 1; j < n ; j++){
+                if(i % j == 0){
+                    count++;
+                }
+            }
+            if(count == 2){
+                System.out.println(i);
+            }
+        }
+    }
+}
