960a8c95ba3c4d94514905d819e46535614ad471
Samir Phad <samirphad4076@gmail.com>
2026-08-27 23:47:01 +0530

    Add Star Pattern program

---
+++ Logical Program/Star-Pattern1/Star11.java
@@ -0,0 +1,18 @@
+// ##**
+// ##**
+// ##**
+// ##**
+
+public class Star11{
+    public static void main(String[] args) {
+        for (int i = 0; i <=4; i++) {
+            for (int j = 1; j <= 2; j++) {
+                System.out.print("#");
+            }
+            for (int k = 1; k <= 2; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
