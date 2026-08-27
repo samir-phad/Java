8a8ad1ca65767b174845541ffe019db2f68536c4
Samir Phad <samirphad4076@gmail.com>
2026-08-27 23:51:56 +0530

    Add Star12 pattern

---
+++ Logical Program/Star-Pattern1/Star12.java
@@ -0,0 +1,19 @@
+//  ###*
+//  ##**
+//  #***
+//  ****
+
+
+public class Star12 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 4; i++) {
+            for (int j = 1; j <= 4-i; j++) {
+                System.out.print("#");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
