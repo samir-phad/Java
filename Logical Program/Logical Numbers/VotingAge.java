e4fd6fd4d509960f5f1022df65af5ae4478e5be8
Samir Phad <samirphad4076@gmail.com>
2026-09-01 22:28:41 +0530

    Check Voting age in java

---
+++ Logical Program/Logical Numbers/VotingAge.java
@@ -0,0 +1,12 @@
+public class VotingAge {
+    public static void main(String[] args) {
+
+        int age = 20;
+
+        if (age >= 18) {
+            System.out.println("Eligible for voting");
+        } else {
+            System.out.println("Not eligible for voting");
+        }
+    }
+}
