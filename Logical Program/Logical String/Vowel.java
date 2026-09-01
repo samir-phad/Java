2967bca7f7e464c5cdcd7ef727706064b83ff81f
Samir Phad <samirphad4076@gmail.com>
2026-09-01 22:17:59 +0530

    Check Latter is Vowel or Consonant

---
+++ Logical Program/Logical String/Vowel.java
@@ -0,0 +1,17 @@
+// Check latter is Vowel or Consonant
+
+public class Vowel {
+    public static void main(String[] args) {
+
+        char ch = 'a';
+
+        if (ch == 'a' || ch == 'e' || ch == 'i' ||
+            ch == 'o' || ch == 'u') {
+
+            System.out.println("Vowel");
+
+        } else {
+            System.out.println("Consonant");
+        }
+    }
+}
