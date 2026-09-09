8af083e60f85103175c4f144606ea562ee54d987
Samir Phad <samirphad4076@gmail.com>
2026-09-09 20:27:51 +0530

    Sum Of Digits

---
+++ Logical Program/Logical Numbers/SumOfDigits.java
@@ -0,0 +1,16 @@
+public class SumOfDigits {
+    public static void main(String[] args) {
+
+        int n = 1234;
+        int sum = 0;
+
+        while (n != 0) {
+
+            int digit = n % 10;
+            sum = sum + digit;
+            n = n / 10;
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
