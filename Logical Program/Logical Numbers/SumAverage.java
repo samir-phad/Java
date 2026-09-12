fb315348073fe2bb4bd3c89181c8b3378d2f48b0
Samir Phad <samirphad4076@gmail.com>
2026-09-12 20:28:06 +0530

    Sum Average

---
+++ Logical Program/Logical Numbers/SumAverage.java
@@ -0,0 +1,14 @@
+public class SumAverage {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        int sum = a + b + c;
+        double average = sum / 3.0;
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Average = " + average);
+    }
+}
