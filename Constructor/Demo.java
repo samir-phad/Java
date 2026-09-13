24e91ecbdf0a6b8bdf0b9cfe39b48c549247f365
Samir Phad <samirphad4076@gmail.com>
2026-09-13 18:57:02 +0530

    Constructor Create & Call

---
+++ Constructor/Demo.java
@@ -0,0 +1,15 @@
+public class Demo {
+    // Constructor Create
+    public Demo(){
+        int a = 15;
+        int b = 15;
+        System.out.println("Multiplication using Constructor");
+        System.out.println(" 15 " + "*" + " 15 " + " = " +(a * b));
+    }
+
+    public static void main(String[] args) {
+        
+        // Object create
+        Demo d = new Demo(); // Call automatically
+    }
+}
